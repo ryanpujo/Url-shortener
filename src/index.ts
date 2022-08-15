2f01c40d679cbfaa09915e20cb71fbd86a783254
Ryan Pujo <pujotriantoro@gmail.com>
2022-08-15 21:08:54 +0700

    add one functionality to retrieve urls

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,9 @@ const port: number = Number(process.env.PORT) || 5000;
 const connect = async () => {
   await mongoose.connect(`${process.env.MONGODB_URL}`);
 };
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello world');
+app.get('/', async (req: Request, res: Response) => {
+  const urls = await Url.find();
+  res.json(urls);
 });
 app.get('/:short', async (req: Request, res: Response, next: NextFunction) => {
   const url = await Url.findOne({ shortenedUrl: req.params.short });
